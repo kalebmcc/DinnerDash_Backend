925b75cbb45838920b2296c24533193bc3582d99
kalebmcc <kalebl33mccullough@gmail.com>
2021-10-31 00:27:31 -0500

    added ids

--- dinnerdash/serializers.py
+++ dinnerdash/serializers.py
@@ -7,27 +7,6 @@ class RecipeSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ('title', 'category', 'ingredients', 'description', "image", 'owner' )
+        fields = ('id','title', 'category', 'ingredients', 'description', "image", 'owner' )
 
 
-# class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
-#     # reviews = serializers.HyperlinkedRelatedField(
-#     #     view_name='review_detail',
-#     #     many=True,
-#     #     read_only=True
-#     # )
-
-#     # if you want to embed reviews in restaurant json response
-#     reviews = ReviewSerializer(
-#         many=True,
-#         read_only=True
-#     )
-
-#     restaurant_url = serializers.ModelSerializer.serializer_url_field(
-#         view_name='restaurant_detail')
-#     owner = serializers.ReadOnlyField(source='owner.username')
-
-#     class Meta:
-#         model = Restaurant
-#         fields = ('id', 'name', 'cuisine', 'reviews',
-#                   'restaurant_url', 'owner' )
